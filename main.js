2fc9b2c1e35f5b3b0333e6d79a609f5df3da05e2
Jurandi Junior <jurandijr20@gmail.com>
2023-01-27 18:44:26 -0300

    Funções anônimas

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,7 +9,10 @@ let contador = 0;
 
 //enquanto 
 while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+
+    listaDeTeclas[contador].onclick = function() {
+        tocaSom('#som_tecla_pom');
+    }
     contador = contador + 1;
 
     console.log(contador);
