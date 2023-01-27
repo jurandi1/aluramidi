1f377dd9ab59b0cc02971938e49e461a6ef0c4e5
Jurandi Junior <jurandijr20@gmail.com>
2023-01-27 19:35:09 -0300

    Eventos do teclado(Enter e Space)

--- main.js
+++ main.js
@@ -16,4 +16,18 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function (evento) {
+       
+        console.log(evento.code); 
+
+        if (evento.code === 'Space' || evento.code === 'Enter') { 
+            tecla.classList.add('ativa');
+       }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+
 }
